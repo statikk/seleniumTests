84ef94ab3c3856dec11e99ee44d04b48210155cc
OTimoshyn <aneloder@gmail.com>
2016-11-01 16:54:29 +0200

    commit

---
+++ src/main/java/test01.java
@@ -0,0 +1,13 @@
+/**
+ * Created by otimoshyn on 11/1/16.
+ */
+public class test01 {
+
+    public static void main (String[] args){
+
+        System.setProperty("webdriver.gecko.driver", "/Users/otimoshyn/Automation/geckodriver");
+
+
+
+    }
+}
