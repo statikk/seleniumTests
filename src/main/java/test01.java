a1d9e98e4a4db4dfb89a46c99a4eff92bd7d5a44
OTimoshyn <aneloder@gmail.com>
2016-11-02 09:58:53 +0200

    commit

--- src/main/java/test01.java
+++ src/main/java/test01.java
@@ -1,3 +1,8 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Created by otimoshyn on 11/1/16.
  */
@@ -5,7 +10,17 @@ public class test01 {
 
     public static void main (String[] args){
 
-        System.setProperty("webdriver.gecko.driver", "/Users/otimoshyn/Automation/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "/Users/otimoshyn/Automation/geckodriver"); // Add dependency to pom.xml
+
+        WebDriver driver = new FirefoxDriver(); // Add webdriver
+
+        driver.get("https://visacenter.ca"); // Open URL
+
+        WebElement searchfield = driver.findElement(By.cssSelector("")); // Find element
+
+
+
+
 
 
 
